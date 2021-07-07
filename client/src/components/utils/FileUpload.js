fdae087a4d9d4ea3a10b03d5625d9fb5215c905e
dw1219 <ymg886@naver.com>
2021-07-07 21:03:02 +0900

    heroku deploy test

--- client/src/components/utils/FileUpload.js
+++ client/src/components/utils/FileUpload.js
@@ -66,7 +66,7 @@ function FileUpload(props) {
             <div style={{ display: 'flex', width: '350px', height: '240px', overflowX: 'scroll' }}>
                 {Images.map((image, index) => (
                     <div onClick={() => deleteHandler(image)} key={index}>
-                        <img style={{ minWidth: '300px', width: '300px', height: '240px' }} src={`http://localhost:5000/${image}`} />
+                        <img style={{ minWidth: '300px', width: '300px', height: '240px' }} src={`https://safe-anchorage-40147.herokuapp.com/${image}`} />
                     </div>
                 ))}
             </div>
