7d6514c425bf144213438693e6edd2c300f20e4c
dw1219 <ymg886@naver.com>
2021-07-08 14:49:43 +0900

    heroku deploy test

--- client/src/components/views/HistoryPage/HistoryPage.js
+++ client/src/components/views/HistoryPage/HistoryPage.js
@@ -5,32 +5,35 @@ function HistoryPage(props) {
 
     return (
         <div style={{ width: '80%', margin: '3rem auto' }}>
-            <div style={{ textAlign: 'center' }}>
+            <div style={{ display: 'flex', justifyContent: 'center' }}>
                 <h1>History</h1>
             </div>
             <br />
-
-            <table>
-                <thead>
-                    <tr>
-                        <th>구매번호</th>
-                        <th>가격</th>
-                        <th>수량</th>
-                        <th>구매날짜</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {props.user.userData && props.user.userData.history.map(item => (
-                        <tr key={item.id}>
-                            <td>{item.id}</td>
-                            <td>{item.price}원</td>
-                            <td>{item.quantity}</td>
-                            <td>{item.dateOfPurchase}</td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-        </div>
+            <div style={{ display: 'flex', justifyContent: 'center' }}>
+                <div>
+                    <table width="70%" align="center">
+                        <thead>
+                            <tr>
+                                <th>구매번호</th>
+                                <th>가격</th>
+                                <th>수량</th>
+                                <th>구매날짜</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {props.user.userData && props.user.userData.history.map(item => (
+                                <tr key={item.id}>
+                                    <td>{item.id}</td>
+                                    <td>{item.price}원</td>
+                                    <td>{item.quantity}</td>
+                                    <td>{item.dateOfPurchase}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div >
     )
 }
 
