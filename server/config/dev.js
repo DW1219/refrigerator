cc0cf4fb13f9e9b30c7351c4be464ec9181cf0de
dw1219 <ymg886@naver.com>
2021-10-13 14:21:33 +0900

    add dev.js

---
+++ server/config/dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+  //mongoURI: 'mongodb+srv://dongwoo19:lge1219@shoppingdw.iva82.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+    mongoURI: 'mongodb+srv://dw:lge1219@cluster0.q7t30.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+}
